f8604f91e2d2e588dd6f56b43142497e9c041301
space2pacman <space2pacman@gmail.com>
2025-10-20 21:04:57 +0300

    fix: formula after cast skill reaction

--- core/states/CastState.js
+++ core/states/CastState.js
@@ -40,7 +40,7 @@ class CastState extends BaseState {
     // }
 
     if (this.character.isCasting) {
-      if ((Date.now() - this.character.castTimestamp) > 4000 * 0.87264) { // ? AtkSpdMultiplier
+      if ((Date.now() - this.character.castTimestamp) > 4000 * (1.1 - (0.0006 * 333))) { // ? AtkSpdMultiplier // (baseCastTime * (1.1 - 0.0006 * magicSpeed));?
         if (skillId === 1177) {
           entity.hp = entity.hp - 30;
 
