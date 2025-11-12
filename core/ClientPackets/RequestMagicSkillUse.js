4e476bd577881c9fbd1addc64648115af4d49ac3
space2pacman <space2pacman@gmail.com>
2025-11-12 20:51:05 +0300

    fix magic attack npc

--- core/ClientPackets/RequestMagicSkillUse.js
+++ core/ClientPackets/RequestMagicSkillUse.js
@@ -1,6 +1,8 @@
 const ClientPacket = require("./ClientPacket");
 const playersManager = require('./../Managers/PlayersManager');
 const skillsManager = require('./../Managers/SkillsManager');
+const serverPackets = require('./../ServerPackets/serverPackets');
+const entitiesManager = require('./../Managers/EntitiesManager');
 
 class RequestMagicSkillUse {
   constructor(client, packet) {
@@ -28,8 +30,15 @@ class RequestMagicSkillUse {
 
   async _init() {
     const player = playersManager.getPlayerByClient(this._client);
+    const entity = entitiesManager.getEntityByObjectId(player.target);
     //const npc = npcManager.getNpcByObjectId(player.target);
     
+    if (entity.canBeAttacked === 0) {
+      this._client.sendPacket(new serverPackets.ActionFailed()); // fix?
+
+      return;
+    }
+
     if (player.isCasting) {
       return;
     }
