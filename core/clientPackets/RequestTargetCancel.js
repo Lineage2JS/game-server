8902c5191d65d5863a1c74ce80edf8b5c18c4ea4
space2pacman <space2pacman@gmail.com>
2026-03-30 19:04:53 +0300

    rework: RequestItemList, RequestShortCutReg, RequestTargetCancel

--- core/clientPackets/RequestTargetCancel.js
+++ core/clientPackets/RequestTargetCancel.js
@@ -2,14 +2,14 @@ const serverPackets = require('./../ServerPackets/serverPackets');
 const ClientPacketNew = require("./ClientPacketNew");
 
 class RequestTargetCanceled extends ClientPacketNew {
-	handle() {
+  handle() {
     const client = this.getClient();
     const player = this.getPlayer();
 
     client.sendPacket(new serverPackets.TargetUnselected(player));
 
-		player.target = null;
-	}
+  player.target = null;
+  }
 }
 
 module.exports = RequestTargetCanceled;
