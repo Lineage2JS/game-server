3c566261847a1dd4432c479f302144a9b5323355
space2pacman <space2pacman@gmail.com>
2026-05-07 18:01:32 +0300

    remove comments

--- core/Models/Npc.js
+++ core/Models/Npc.js
@@ -6,30 +6,6 @@ const FollowState = require('./../states/FollowState');
 const PickupState = require('./../states/PickupState');
 const DeadState = require('./../states/DeadState');
 
-//
-//const entitiesManager = require('./../Managers/EntitiesManager'); // fix?
-
-//
-// function moveCloser(x1, y1, x2, y2, distance) {
-//   // Вычисляем разницу между координатами
-//   let dx = x2 - x1;
-//   let dy = y2 - y1;
-
-//   // Вычисляем расстояние между точками
-//   let dist = Math.sqrt(dx * dx + dy * dy);
-
-//   // Нормализуем вектор разницы
-//   let nx = dx / dist;
-//   let ny = dy / dist;
-
-//   // Перемещаем точку (x2, y2) ближе на заданное расстояние
-//   let newX = x2 - nx * distance;
-//   let newY = y2 - ny * distance;
-
-//   return { x: newX, y: newY };
-// }
-//
-
 class Npc extends Character {
   constructor() {
     super();
@@ -68,13 +44,9 @@ class Npc extends Character {
     this.getMagicalSpeed = 333; // fix
     
     //
-    this.lastTimeTick = 0;
     this.coordinates = null;
 
     this.lastAttackTimestamp = 0;
-    //
-    this.payloadAttack = null; // fix
-    //
 
     //
     this.additionalMakeMultiList = [];
